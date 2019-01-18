ba2df571e8bfbaa877ee4b8028c6e5de3be162a1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-01-18 13:19:50 -0500

    Initial commit.

--- .ember-cli.js
+++ .ember-cli.js
@@ -1,4 +1,10 @@
-{
+'use strict';
+
+process.env.EMBER_CLI_MODULE_UNIFICATION = true;
+process.env.EMBER_CLI_BROCCOLI_2 = true;
+process.env.EMBER_CLI_SYSTEM_TEMP = true;
+
+module.exports = {
   /**
     Ember CLI sends analytics information by default. The data is completely
     anonymous, but there are times when you might want to disable this behavior.
