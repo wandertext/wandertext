00a532928ecc2e9cde4269d8e6d71a18294b0aac
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-02-17 09:57:38 -0500

    Absolutify link to waw.

--- src/ui/components/card-header/component.js
+++ src/ui/components/card-header/component.js
@@ -1,3 +1,6 @@
 import Component from "@ember/component";
+import config from "../../../../config/environment";
 
-export default class CardHeaderComponent extends Component {}
+export default class CardHeaderComponent extends Component {
+  rootURL = config.rootURL
+}
