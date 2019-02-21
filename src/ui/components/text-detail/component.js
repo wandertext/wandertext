72e95e9b3a45d60ce6aca808eeb93ac6d92a0922
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-02-21 16:09:11 -0500

    count distinct places.

--- src/ui/components/text-detail/component.js
+++ src/ui/components/text-detail/component.js
@@ -1,11 +1,12 @@
 import Component from "@ember/component";
 import { computed } from "@ember-decorators/object";
 import { inject as service } from "@ember-decorators/service";
+import { uniq } from "@ember-decorators/object/computed";
 
 export default class TextDetailComponent extends Component {
   @service data;
 
-  distinctPlaces = [];
+  @uniq("places") distinctPlaces;
 
   @computed("distinctPlaces")
   get distinctPlacesCount() {
@@ -19,10 +20,14 @@ export default class TextDetailComponent extends Component {
     return this.entries.length;
   }
 
+  places = [];
+
   async didInsertElement() {
     const text = await this.data.getTextBySlug(this.get("slug"));
     this.set("text", text);
     const entries = await this.data.getEntriesByText(text.id);
     this.set("entries", entries);
+    const places = await entries.map(entry => entry.place);
+    this.set("places", places);
   }
 }
