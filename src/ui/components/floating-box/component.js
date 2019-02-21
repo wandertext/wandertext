ecb55d2ad7cb85cd0dbf958b33ec26ee69352259
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-02-21 17:00:23 -0500

    Redo floating-box div definition.

--- src/ui/components/floating-box/component.js
+++ src/ui/components/floating-box/component.js
@@ -2,7 +2,12 @@ import Component from "@ember/component";
 import { action } from "@ember-decorators/object";
 
 export default class FloatingBoxComponent extends Component {
+
   cardCollapsed = false;
+
+  classNames = ["m-3 card"];
+
+  elementId = "floating-box";
 
   @action
   toggleContent() {
