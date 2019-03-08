bd7eacbdd6119d68d67bf47e2bab4f5e167766c9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-08 11:33:15 -0500

    Turn floating box into a material card.

--- src/ui/components/floating-box/component.js
+++ src/ui/components/floating-box/component.js
@@ -4,10 +4,6 @@ import { action } from "@ember-decorators/object";
 export default class FloatingBoxComponent extends Component {
   cardCollapsed = false;
 
-  classNames = ["m-3 card"];
-
-  elementId = "floating-box";
-
   @action
   toggleContent() {
     this.toggleProperty("cardCollapsed");
