71985eaa3c09bb6f21a2fa278abea076b3bfdfb1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-09 19:38:39 -0500

    Add some navigation to sidenav. Adds to #3.

--- src/ui/components/sidebar-content/component.js
+++ src/ui/components/sidebar-content/component.js
@@ -1,3 +1,10 @@
 import Component from "@ember/component";
 
-export default class SidebarContentComponent extends Component {}
+export default class SidebarContentComponent extends Component {
+  sidenavList = [
+    { text: "Home", icon: "home", route: "index" },
+    { text: "Places", icon: "place", route: "places" },
+    { text: "Texts", icon: "library_books", route: "texts" },
+    { text: "About", icon: "info", route: "about" }
+  ];
+}
