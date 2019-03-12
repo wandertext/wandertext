880ae0cac85d727a8c1b9832f25ab222c8a0d034
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-12 13:26:59 -0400

    Change text of home button on sidenav.

--- src/services/sidenav.js
+++ src/services/sidenav.js
@@ -2,7 +2,7 @@ import Service from "@ember/service";
 
 export default class SidenavService extends Service {
   linkList = [
-    { text: "Home", icon: "home", route: "index" },
+    { text: "Wandertext", icon: "home", route: "index" },
     { text: "Places", icon: "place", route: "places" },
     { text: "Texts", icon: "library_books", route: "texts" },
     { text: "About", icon: "info", route: "about" }
