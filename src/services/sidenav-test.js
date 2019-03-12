d0a2108adb3cc9ea562c04ee72cac4cc71990047
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-12 13:04:29 -0400

    Move sidenav to service.

---
+++ src/services/sidenav-test.js
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { setupTest } from "ember-mocha";
+
+describe("Unit | Service | sidenav", function() {
+  setupTest();
+
+  // Replace this with your real tests.
+  it("exists", function() {
+    const service = this.owner.lookup("service:sidenav");
+    expect(service).to.be.ok;
+  });
+});
