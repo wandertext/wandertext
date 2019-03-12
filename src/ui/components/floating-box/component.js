7c18bc95d9066749b95be2e4cbf69fc878e53ebb
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-12 13:12:28 -0400

    Add floatingbox reference to sidenav on menu button click.

--- src/ui/components/floating-box/component.js
+++ src/ui/components/floating-box/component.js
@@ -1,6 +1,25 @@
 import Component from "@ember/component";
+import { action } from "@ember-decorators/object";
 import { inject as service } from "@ember/service";
 
 export default class FloatingBoxComponent extends Component {
   @service card;
+
+  @service router;
+
+  @service sidenav;
+
+  @service paperSidenav;
+
+  @action
+  toggleAction() {
+    this.paperSidenav.toggle("left");
+    if (this.sidenav.linkList[0].text !== this.card.title) {
+      this.sidenav.linkList.unshiftObject({
+        text: this.card.title,
+        icon: "info",
+        route: this.router.currentRouteName
+      });
+    }
+  }
 }
