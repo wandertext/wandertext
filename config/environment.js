24807e40fc0c8554f6d67909bb4607b3ce332406
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-22 09:57:17 -0400

    Enable some canary flags and fix metal tracked props!

--- config/environment.js
+++ config/environment.js
@@ -15,7 +15,10 @@ module.exports = function(environment) {
     },
     EmberENV: {
       FEATURES: {
-        EMBER_NATIVE_DECORATOR_SUPPORT: true
+        EMBER_NATIVE_DECORATOR_SUPPORT: true,
+        EMBER_METAL_TRACKED_PROPERTIES: true,
+        EMBER_GLIMMER_ANGLE_BRACKET_NESTED_LOOKUP: true,
+        EMBER_GLIMMER_ANGLE_BRACKET_BUILT_INS: true
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
