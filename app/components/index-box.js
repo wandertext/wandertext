6ab576b58a17f44f83fe8355756ce8c90fab6835
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-25 13:53:55 -0400

    Trim out data layer for index-box

--- app/components/index-box.js
+++ app/components/index-box.js
@@ -2,16 +2,7 @@ import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 
 export default class IndexBoxComponent extends Component {
-  @service data;
-
   @service card;
-
-  constructor(...args) {
-    super(...args);
-    if (!this.data.docs) {
-      this.data._setDocs();
-    }
-  }
 
   didInsertElement() {
     this.card.reset();
