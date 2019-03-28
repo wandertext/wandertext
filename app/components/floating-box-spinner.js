b27d56492999fa49d91176a89116e49c2ca1473b
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-27 20:08:52 -0400

    Remove a possible bit of ickiness in setting the style for the spinner.

--- app/components/floating-box-spinner.js
+++ app/components/floating-box-spinner.js
@@ -8,6 +8,10 @@ export default class FloatingBoxSpinnerComponent extends Component {
     return 0.5 * this.height;
   }
 
+  get style() {
+    return `min-height: ${this.height}px;`;
+  }
+
   constructor(...args) {
     super(...args);
     this.height = 0.4 * window.innerHeight;
