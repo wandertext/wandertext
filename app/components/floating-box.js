29f4e1257fe937c2a11e5b2186fd739cfd8b2b9d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-31 22:07:57 -0400

    Remove ember-decorators

--- app/components/floating-box.js
+++ app/components/floating-box.js
@@ -1,5 +1,5 @@
 import Component from "@glimmer/component";
-import { action } from "@ember-decorators/object";
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 
 export default class FloatingBoxComponent extends Component {
--- app/components/zoom-control.js
+++ app/components/zoom-control.js
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-import { action } from "@ember-decorators/object";
+import { action } from "@ember/object";
 
 export default class ZoomControlComponent extends Component {
   @service theMap;
