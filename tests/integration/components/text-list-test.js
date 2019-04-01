f776c84abd30bb90d7e9da1ba523caf2c28e9089
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-03-31 20:30:08 -0400

    Move text-listings into text-list

---
+++ tests/integration/components/text-list-test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { setupRenderingTest } from "ember-mocha";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
+
+describe("Integration | Component | text-list", function() {
+  setupRenderingTest();
+
+  it("renders as #text-list", async function() {
+    await render(hbs`<TextList />`);
+    expect(this.element.querySelector("#text-list")).to.be.ok;
+  });
+});
