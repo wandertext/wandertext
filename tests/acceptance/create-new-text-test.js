e5dde0ec3976805ea183dcaa8e1f79da9bb926db
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-04-03 12:39:50 -0400

    Get a single word from lorem for the entrySort.

--- tests/acceptance/create-new-text-test.js
+++ tests/acceptance/create-new-text-test.js
@@ -22,7 +22,7 @@ describe("Acceptance | create new Text", function() {
   it("fills out the form, makes a Text, & transitions to texts/index", async function() {
     const name = faker.commerce.productName();
     const slug = faker.helpers.slugify(name) + "-1976";
-    const entrySort = faker.random.word();
+    const entrySort = faker.lorem.word();
     const store = this.owner.lookup("service:store");
     await visit("/texts/new");
     await fillIn("#input-name", name);
