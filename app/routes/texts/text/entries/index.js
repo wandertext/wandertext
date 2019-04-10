b3531a11ef8b7b656681799ac6d774b89b83bbca
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-04-09 23:01:22 -0400

    Deliver entries

--- app/routes/texts/text/entries/index.js
+++ app/routes/texts/text/entries/index.js
@@ -1,12 +1,8 @@
 import Route from "@ember/routing/route";
 
 export default class TextsTextEntriesIndexRoute extends Route {
-  model({ slug }) {
-    console.log("slug", slug);
-    console.log("paramsfor", this);
-    const text = this.modelFor("show");
-    console.log("text", text);
-    console.log("text.entries", text.entries);
-    // Return entries;
+  model() {
+    const text = this.modelFor("texts/text");
+    return text.entries;
   }
 }
