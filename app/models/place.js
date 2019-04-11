18aefd445d68fa351f17b81ad731fad8ba06b161
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-04-11 12:06:04 -0400

    Add geometry and properties to Place.

--- app/models/place.js
+++ app/models/place.js
@@ -4,9 +4,11 @@ import DS from "ember-data";
 const { attr, hasMany } = DS;
 
 export default class Place extends Model {
-  @attr("string") name;
-
   @attr("string", { defaultValue: "Feature" }) type;
+
+  @attr() geometry;
+
+  @attr() properties;
 
   @hasMany("entry") entries;
 }
