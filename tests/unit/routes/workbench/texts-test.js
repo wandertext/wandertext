c084d321b60b5af660702c8a2d68e1dd3de52dca
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-04-29 12:54:48 -0400

    Fix up route unit tests behind workbench

--- tests/unit/routes/workbench/texts-test.js
+++ tests/unit/routes/workbench/texts-test.js
@@ -2,11 +2,11 @@ import { expect } from "chai";
 import { describe, it } from "mocha";
 import { setupTest } from "ember-mocha";
 
-describe("Unit | Route | texts", function() {
+describe("Unit | Route | workbench/texts", function() {
   setupTest();
 
   it("exists", function() {
-    const route = this.owner.lookup("route:texts");
+    const route = this.owner.lookup("route:workbench.texts");
     expect(route).to.be.ok;
   });
 });
--- tests/unit/routes/workbench/texts/text/entries-test.js
+++ tests/unit/routes/workbench/texts/text/entries-test.js
@@ -2,11 +2,11 @@ import { expect } from "chai";
 import { describe, it } from "mocha";
 import { setupTest } from "ember-mocha";
 
-describe("Unit | Route | texts/text/entries", function() {
+describe("Unit | Route | workbench/texts/text/entries", function() {
   setupTest();
 
   it("exists", function() {
-    const route = this.owner.lookup("route:texts/text/entries");
+    const route = this.owner.lookup("route:workbench/texts/text/entries");
     expect(route).to.be.ok;
   });
 });
