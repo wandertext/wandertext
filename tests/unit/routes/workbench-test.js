6ca6ecea6ad0ad290481a6f5eb061878c9868a9d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-02 14:25:25 -0400

    Add workbench route and make it auth only.

---
+++ tests/unit/routes/workbench-test.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { setupTest } from "ember-mocha";
+
+describe("Unit | Route | workbench", function() {
+  setupTest();
+
+  it("exists", function() {
+    const route = this.owner.lookup("route:workbench");
+    expect(route).to.be.ok;
+  });
+});
