432b88f6274d6f658142456583d031c197b8cc6e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-06 14:19:07 -0400

    Incorporate some ember-concurrency but whatever.

--- app/routes/workbench/texts/text.js
+++ app/routes/workbench/texts/text.js
@@ -1,13 +1,19 @@
 import Route from "@ember/routing/route";
+import { task } from "ember-concurrency-decorators";
 import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default class TextsTextRoute extends Route.extend(
   AuthenticatedRouteMixin
 ) {
-  async model({ slug }) {
-    const texts = await this.store.loadRecords("text", {
+  model({ slug }) {
+    return this.textTask.perform(slug);
+  }
+
+  @task
+  textTask = function*(slug) {
+    const texts = yield this.store.loadRecords("text", {
       filter: { slug }
     });
     return texts.firstObject;
-  }
+  };
 }
