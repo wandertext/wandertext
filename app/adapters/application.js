165d03631f9cd5f3b21fdef457d7cbb8e0ccaa50
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-06 09:34:40 -0400

    Trim down adapter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,43 +4,24 @@ import PouchDB from "pouchdb";
 import config from "wandertext/config/environment";
 
 export default class ApplicationAdapter extends Adapter {
-  @tracked db;
+  @tracked db = new PouchDB(config.emberPouch.localDb);
 
-  @tracked remoteDb;
+  @tracked remoteDb = null;
 
   constructor(...args) {
     super(...args);
-    this.db = new PouchDB(config.emberPouch.localDb);
     if (config.emberPouch.remoteDb) {
-      this.remoteDb = new PouchDB(config.emberPouch.remoteDb, {
-        fetch(url, opts) {
-          opts.credentials = "include";
-          return PouchDB.fetch(url, opts);
-        }
-      });
+      this.remoteDb = new PouchDB(config.emberPouch.remoteDb);
+      // This.remoteDb = new PouchDB(config.emberPouch.remoteDb, {
+      //   fetch(url, opts) {
+      //     opts.credentials = "include";
+      //     return PouchDB.fetch(url, opts);
+      //   }
+      // });
       this.db.sync(this.remoteDb, {
         live: true,
         retry: true
       });
-    } else {
-      this.remoteDb = null;
     }
-
-    this.db.createIndex({
-      index: {
-        fields: ["data.name"],
-        name: "data-name",
-        ddoc: "data-name",
-        type: "json"
-      }
-    });
-    this.db.createIndex({
-      index: {
-        fields: ["data.slug"],
-        name: "data-slug",
-        ddoc: "data-slug",
-        type: "json"
-      }
-    });
   }
 }
