27787692ae6e9b6ea7b7e7a7f64c68af266022ec
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-09 18:38:20 -0400

    Wire up working table of entries in texts/text.

--- app/components/text-entry-list.js
+++ app/components/text-entry-list.js
@@ -1,3 +1,43 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { capitalize } from "@ember/string";
 
-export default class TextEntryListComponent extends Component {}
+export default class TextEntryListComponent extends Component {
+  @tracked columns = [];
+
+  @tracked rows = [];
+
+  constructor(...args) {
+    super(...args);
+    this._buildColumns(this.args.text);
+    this._buildRows(this.args.text);
+  }
+
+  _buildColumns(text) {
+    this.columns.pushObject({ name: "ID", valuePath: "id" });
+    text.entryProperties.forEach(propObj => {
+      const name = propObj.inputLabel || capitalize(propObj.name);
+      const valuePath = `eProp${propObj.name}`;
+      this.columns.pushObject({ valuePath, name });
+    });
+    this.columns.pushObject({
+      valuePath: "attestedName",
+      name: "Attested Name"
+    });
+    this.columns.pushObject({ valuePath: "createdOn", name: "Created On" });
+  }
+
+  _buildRows({ entries }) {
+    entries.forEach(entry => {
+      const row = {};
+      this.columns.forEach(({ valuePath }) => {
+        if (valuePath.startsWith("eProp")) {
+          row[valuePath] = entry.properties[valuePath.replace(/^eProp/, "")];
+        } else {
+          row[valuePath] = entry[valuePath];
+        }
+      });
+      this.rows.pushObject(row);
+    });
+  }
+}
--- app/routes/workbench/texts/text/entries/index.js
+++ app/routes/workbench/texts/text/entries/index.js
@@ -2,7 +2,6 @@ import Route from "@ember/routing/route";
 
 export default class TextsTextEntriesIndexRoute extends Route {
   model() {
-    const text = this.modelFor("workbench/texts/text");
-    return text.entries;
+    return this.modelFor("workbench/texts/text");
   }
 }
