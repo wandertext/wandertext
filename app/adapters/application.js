37a911162b028db976bbe6a19bbef656216edac1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-09 14:20:14 -0400

    knock out local db for now.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,27 +1,30 @@
+/* eslint capitalized-comments: 0 */
 import { Adapter } from "ember-pouch";
 import { tracked } from "@glimmer/tracking";
 import PouchDB from "pouchdb";
 import config from "wandertext/config/environment";
 
 export default class ApplicationAdapter extends Adapter {
-  @tracked db = new PouchDB(config.emberPouch.localDb);
+  // @tracked db = new PouchDB(config.emberPouch.localDb);
+  @tracked db = null;
 
   @tracked remoteDb = null;
 
   constructor(...args) {
     super(...args);
     if (config.emberPouch.remoteDb) {
-      this.remoteDb = new PouchDB(config.emberPouch.remoteDb);
-      // This.remoteDb = new PouchDB(config.emberPouch.remoteDb, {
-      //   fetch(url, opts) {
-      //     opts.credentials = "include";
-      //     return PouchDB.fetch(url, opts);
-      //   }
+      this.db = new PouchDB(config.emberPouch.remoteDb);
+      // this.remoteDb = new PouchDB(config.emberPouch.remoteDb);
+      // // this.remoteDb = new PouchDB(config.emberPouch.remoteDb, {
+      // //   fetch(url, opts) {
+      // //     opts.credentials = "include";
+      // //     return PouchDB.fetch(url, opts);
+      // //   }
+      // // });
+      // this.db.sync(this.remoteDb, {
+      //   live: true,
+      //   retry: true
       // });
-      this.db.sync(this.remoteDb, {
-        live: true,
-        retry: true
-      });
     }
   }
 }
