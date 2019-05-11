b7c7b88b9437d0b81fb5295a4606abe0efeb44d9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-11 18:46:18 -0400

    Add login functionality to index box for now until sidenav is fixed.

--- app/components/index-box.js
+++ app/components/index-box.js
@@ -1,10 +1,35 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
 export default class IndexBoxComponent extends Component {
+  @service router;
+
+  @service store;
+
   @service card;
+
+  @service session;
+
+  @tracked githubUser = null;
 
   didInsertElement() {
     this.card.reset();
+  }
+
+  @action
+  logout() {
+    this.session.invalidate();
+  }
+
+  @action
+  async login() {
+    await this.session.authenticate("authenticator:torii", "github");
+    this.githubUser = await this.store.findRecord("github-user", "#");
+    this.router.transitionTo(
+      "workbench.contributors.contributor",
+      this.githubUser.login
+    );
   }
 }
