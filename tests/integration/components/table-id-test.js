fafbad31456eb8a9b554a01e97f8d4e5ca64a88a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-20 16:32:21 -0400

    Add components for table cells.

---
+++ tests/integration/components/table-id-test.js
@@ -0,0 +1,27 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { setupRenderingTest } from "ember-mocha";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
+
+describe("Integration | Component | table-id", function() {
+  setupRenderingTest();
+
+  it.skip("renders", async function() {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<TableId />`);
+
+    expect(this.element.textContent.trim()).to.equal("");
+
+    // Template block usage:
+    await render(hbs`
+      <TableId>
+        template block text
+      </TableId>
+    `);
+
+    expect(this.element.textContent.trim()).to.equal("template block text");
+  });
+});
