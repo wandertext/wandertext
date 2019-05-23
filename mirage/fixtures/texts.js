e5dd121e48096906414356fa0d67d7aef50fb14c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-23 15:55:53 -0400

    Add two mirage text fixtures.

---
+++ mirage/fixtures/texts.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    name: "Baburnama",
+    slug: "baburnama-1530",
+    markdownBlurb: "markdown blurb for baburnama"
+  },
+  {
+    id: 2,
+    name: "Nightwood",
+    slug: "nightwood-1936",
+    markdownBlurb: "markdown blurb for nightwood"
+  }
+];
