c59f0921dcb61cda2b9cc0eed5bc7669b2d0f20a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-23 14:37:29 -0400

    Remove pouch from adapters. Fixes #146

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,28 +1,5 @@
-/* eslint capitalized-comments: 0 */
-import { Adapter } from "ember-pouch";
-import { tracked } from "@glimmer/tracking";
-import PouchDB from "pouchdb";
-import config from "wandertext/config/environment";
+import DS from "ember-data";
 
-export default class ApplicationAdapter extends Adapter {
-  @tracked db = new PouchDB(config.emberPouch.localDb);
-
-  @tracked remoteDb = null;
-
-  constructor(...args) {
-    super(...args);
-    if (config.emberPouch.remoteDb) {
-      this.remoteDb = new PouchDB(config.emberPouch.remoteDb);
-      this.remoteDb = new PouchDB(config.emberPouch.remoteDb, {
-        fetch(url, opts) {
-          opts.credentials = "include";
-          return PouchDB.fetch(url, opts);
-        }
-      });
-      this.db.sync(this.remoteDb, {
-        live: true,
-        retry: true
-      });
-    }
-  }
-}
+export default DS.JSONAPIAdapter.extend({
+  namespace: "mirage"
+});
