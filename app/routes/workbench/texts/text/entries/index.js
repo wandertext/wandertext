4653188b35b1e71bfb24dd20a37c785027d79645
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-05-29 14:24:31 -0400

    Sideload entries' place alongside entries. Fixes #155

--- app/routes/workbench/texts/text/entries/index.js
+++ app/routes/workbench/texts/text/entries/index.js
@@ -2,6 +2,6 @@ import Route from "@ember/routing/route";
 
 export default class TextsTextEntriesIndexRoute extends Route {
   model() {
-    return this.modelFor("workbench/texts/text").sideload("entries");
+    return this.modelFor("workbench/texts/text").sideload("entries.place");
   }
 }
