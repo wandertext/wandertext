cd6154bde4a19fd009fdb156f3d8146ba537b0f7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-06-19 21:50:02 -0400

    Remove mirage.

--- mirage/models/contributor.js
+++
@@ -1,7 +0,0 @@
-import { Model, hasMany } from "ember-cli-mirage";
-
-export default Model.extend({
-  entries: hasMany("entry", { async: false }),
-  places: hasMany("place", { async: false }),
-  texts: hasMany("text", { async: false })
-});
--- mirage/models/place.js
+++
@@ -1,6 +0,0 @@
-import { Model, hasMany } from "ember-cli-mirage";
-
-export default Model.extend({
-  entries: hasMany("entry", { async: false }),
-  contributors: hasMany("contributor", { async: false })
-});
