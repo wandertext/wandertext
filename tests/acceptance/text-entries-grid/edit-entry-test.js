05143f668500fcebee46ec96c49be63915a60f4d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-06-21 22:36:34 -0400

    Build most of a (failing) test for editing in the grid

---
+++ tests/acceptance/text-entries-grid/edit-entry-test.js
@@ -0,0 +1,53 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import { setupApplicationTest } from "ember-mocha";
+import { visit, fillIn } from "@ember/test-helpers";
+import { authenticateSession } from "ember-simple-auth/test-support";
+import { setupMirage } from "ember-cli-mirage/test-support";
+import { lorem } from "faker";
+
+describe("Acceptance | text/entries grid | edit entry", function() {
+  const hooks = setupApplicationTest();
+  setupMirage(hooks);
+
+  it.only("can edit an entry", async function() {
+    authenticateSession();
+    this.store = this.owner.lookup("service:store");
+    this.currentContributor = this.owner.lookup("service:currentContributor");
+    this.currentContributor.contributor = {
+      id: "contrib-username-id",
+      firstName: "contrib-first",
+      lastName: "contrib-last"
+    };
+    this.text = await this.server.create("text");
+    // Create two dummy entries ahead of time.
+    this.entries = await this.server.createList("entry", 2, {
+      text: this.text,
+      properties: {
+        special: lorem.word(),
+        page: 2
+      }
+    });
+    await visit(`/workbench/texts/${this.text.id}/entries/`);
+    const propSpecialCells = document.querySelectorAll(
+      ".table-edit-input.entry-property-properties-special"
+    );
+    expect(propSpecialCells.length).to.equal(2);
+    const specialCell = propSpecialCells[0];
+    const updatedEntryId = specialCell.classList
+      .toString()
+      .split(" ")
+      .filter(klass => klass.startsWith("entry-id"))[0]
+      .replace("entry-id-", "");
+    await fillIn(specialCell, "new special");
+    await fillIn(
+      document.querySelector(
+        `.entry-property-properties-page.entry-id-${updatedEntryId}`
+      ),
+      "boogie"
+    );
+    const updatedEntry = await this.store.findRecord("entry", updatedEntryId);
+    expect(updatedEntry.properties.special).to.equal("new special");
+    // Contrib should get added to contribs array
+  });
+});
