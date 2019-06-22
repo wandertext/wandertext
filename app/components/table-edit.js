586aa7651d9ef6386cd0cc5cdae6f5176e95967e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-06-21 22:35:26 -0400

    Work on table-edit component to save only when even a nested prop is changed.

--- app/components/table-edit.js
+++ app/components/table-edit.js
@@ -3,12 +3,22 @@ import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 
 export default class TableEditComponent extends Component {
-  @tracked previousValue = null;
+  previousValue = String(this.currentValue);
+
+  get property() {
+    return this.args.property.replace(".", "-");
+  }
+
+  @tracked currentValue = this.args.entry.get(this.args.property) || "";
 
   @action
   saveCell() {
-    if (this.args.entry.hasDirtyAttributes) {
+    if (
+      this.args.entry.hasDirtyAttributes ||
+      this.previousValue !== this.currentValue
+    ) {
       this.args.entry.save();
+      this.previousValue = this.currentValue;
     }
   }
 }
