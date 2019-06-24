f7ec336049997065697d8d3d9a2c61a0ccb41749
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-06-24 13:31:27 -0400

    Bring changeset into table-cell and trick around getting input box to work.

--- app/components/table-cell.js
+++ app/components/table-cell.js
@@ -1,13 +1,18 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
 
 export default class TableCellComponent extends Component {
   @service currentContributor;
 
+  @tracked currentValue = this.args.changeset.get(this.property);
+
   get classes() {
-    return `p-1 cell-${this.property.replace(".", "-")} cell-id-${
-      this.args.entry.id
-    }`;
+    return `p-1 cell-${this.property.replace(
+      ".",
+      "-"
+    )} cell-id-${this.args.changeset.get("id")}`;
   }
 
   get isPlace() {
@@ -28,6 +33,29 @@ export default class TableCellComponent extends Component {
           this.args.column.property.owner !==
             this.currentContributor.contributor.id)
       );
+    }
+
+    return false;
+  }
+
+  @action
+  updateChangeset(inputValue) {
+    // This.currentValue is goofy but setting value as a getter
+    // directly on the input freezes the input and doesn't let you
+    // type in anything new.
+    this.currentValue = inputValue;
+    this.args.changeset.set(this.property, this.currentValue);
+  }
+
+  get changesetErrors() {
+    const { error } = this.args.changeset;
+    const property = this.property.replace(/^properties\./, "");
+    if (property in error) {
+      return error[property];
+    }
+
+    if ("properties" in error && property in error.properties) {
+      return error.properties[property];
     }
 
     return false;
