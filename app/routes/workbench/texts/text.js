f74aa8b9f340d77f040952e33a7911596ec52c9f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-02 13:21:12 -0400

    Add future test for writing privileges on texts.

--- app/routes/workbench/texts/text.js
+++ app/routes/workbench/texts/text.js
@@ -1,9 +1,26 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
+import { inject as service } from "@ember/service";
 
 export default class TextsTextRoute extends WorkbenchRoute {
+  @service currentContributor;
+
   model({ id }) {
     return this.store.findRecord("text", id, {
       include: "entries,entries.place"
     });
   }
+
+  /*
+   * This will work once contributors is an array of IDs, not an array of 
+   * actual Contributor objects. I think.
+  afterModel(model) {
+    if (
+      model.contributors.filter(
+        c => c === this.currentContributor.contributor.id
+      ).length === 0
+    ) {
+      this.transitionTo("workbench.texts");
+    }
+  }
+  */
 }
