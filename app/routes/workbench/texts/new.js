1571ba17c61ee4e3c009b67cd5a57c625fa401e6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-02 13:23:42 -0400

    Don't even give texts/new a second thought. Fixes #223.

--- app/routes/workbench/texts/new.js
+++ app/routes/workbench/texts/new.js
@@ -1,6 +1,10 @@
 import Route from "@ember/routing/route";
 
 export default class TextsNewRoute extends Route {
+  beforeModel() {
+    this.transitionTo("workbench.texts");
+  }
+
   model() {
     return this.store.createRecord("text");
   }
