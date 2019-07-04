60f3def0bba83f594848f5747cdb4a0fdd228f95
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-03 23:38:41 -0400

    Only show public texts even in workbench

--- app/routes/workbench/texts/index.js
+++ app/routes/workbench/texts/index.js
@@ -1,8 +1,11 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
 
 export default class TextsIndexRoute extends WorkbenchRoute {
-  async model() {
-    await this.store.loadRecords("text");
-    return this.store.peekAll("text");
+  model() {
+    return this.store.query("text", {
+      filter: {
+        public: true
+      }
+    });
   }
 }
