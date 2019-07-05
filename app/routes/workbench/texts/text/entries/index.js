4b266e264ae0b9846bf7ef7a91ca4af41ab29d69
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-05 11:46:46 -0400

    Split text and entries into two models when loading text.entries. Fixes #227

--- app/routes/workbench/texts/text/entries/index.js
+++ app/routes/workbench/texts/text/entries/index.js
@@ -1,9 +1,14 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
+import { hash } from "rsvp";
 
 export default class TextsTextEntriesIndexRoute extends WorkbenchRoute {
-  model() {
-    return this.modelFor("workbench/texts/text").sideload(
-      "entries,entries.place,entries.contributors"
+  async model() {
+    const text = await this.modelFor("workbench/texts/text").sideload(
+      "entries,entries.place"
     );
+    return hash({
+      text,
+      entries: text.entries
+    });
   }
 }
