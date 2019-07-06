1e3d4e37dee3a34c0fcbf8abe3673821c74620c4
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-06 02:50:09 -0400

    Trim out apollo from routes

--- app/routes/workbench.js
+++ app/routes/workbench.js
@@ -1,8 +1,6 @@
 import Route from "@ember/routing/route";
-import { RouteQueryManager } from "ember-apollo-client";
 import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default class WorkbenchRoute extends Route.extend(
-  AuthenticatedRouteMixin,
-  RouteQueryManager
+  AuthenticatedRouteMixin
 ) {}
