e68e60d6f1035691441f87a20cb45df50486913f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-06 09:31:16 -0400

    Split model on workbench.index to both have a valid button and a greeting.

--- app/routes/workbench/index.js
+++ app/routes/workbench/index.js
@@ -1,10 +1,13 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
 import { inject as service } from "@ember/service";
+import { hash } from "rsvp";
 
 export default class WorkbenchIndexRoute extends WorkbenchRoute {
   @service currentContributor;
 
-  model() {
-    return this.currentContributor.contributor;
+  async model() {
+    const contributor = await this.currentContributor.contributor;
+    const text = await this.store.findRecord("text", "baburnama-1530");
+    return hash({ contributor, text });
   }
 }
