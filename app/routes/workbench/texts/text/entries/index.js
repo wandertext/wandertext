c785184e359d87b0655c4b4b595c31ea82841fb8
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-08 21:53:44 -0400

    Only deliver @text on entries/index.

--- app/routes/workbench/texts/text/entries/index.js
+++ app/routes/workbench/texts/text/entries/index.js
@@ -1,20 +1,7 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
-import { hash } from "rsvp";
 
 export default class TextsTextEntriesIndexRoute extends WorkbenchRoute {
-  async model() {
-    const text = await this.modelFor("workbench/texts/text");
-    const entries = await this.store.query("entry", {
-      query: ref =>
-        ref
-          .where("text", "==", "baburnama-1530")
-          .orderBy("properties.folio", "asc")
-          .orderBy("properties.sequence", "asc")
-          .limit(20)
-    });
-    return hash({
-      text,
-      entries
-    });
+  model() {
+    return this.modelFor("workbench/texts/text");
   }
 }
