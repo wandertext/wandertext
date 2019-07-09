43fd75cbf251d7fca3fa3c3f8437efe6c073b7ab
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-09 19:52:03 -0400

    Fix up modal map and put in some concurrency goodness into place. Fixes #162

--- app/components/table-cell.js
+++ app/components/table-cell.js
@@ -3,6 +3,7 @@ import { tracked } from "@glimmer/tracking";
 import { action, get } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { htmlSafe } from "@ember/string";
+import { task } from "ember-concurrency";
 
 export default class TableCellComponent extends Component {
   @service notify;
@@ -30,6 +31,13 @@ export default class TableCellComponent extends Component {
 
   get isDate() {
     return /(creat|modifi)edOn/.test(this.args.column.valuePath);
+  }
+
+  constructor(...args) {
+    super(...args);
+    if (this.isPlace) {
+      this.fetchPlace.perform();
+    }
   }
 
   @action
@@ -75,4 +83,16 @@ export default class TableCellComponent extends Component {
 
     return false;
   }
+
+  @tracked isMappable = false;
+
+  @tracked place = null;
+
+  @task(function*() {
+    this.place = yield this.args.changeset.get("place");
+    if (this.place.latitude && this.place.longitude) {
+      this.isMappable = true;
+    }
+  })
+  fetchPlace;
 }
