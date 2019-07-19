b4d297c2c08436d9f9b85136c4dca7f134bec97d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-07-19 17:06:38 -0400

    clean up post github and apollo removal.

--- app/routes/workbench/contributors/contributor.js
+++ app/routes/workbench/contributors/contributor.js
@@ -1,9 +1,5 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
-import query from "wandertext/gql/queries/contributor";
 
 export default class WorkbenchContributorsContributorRoute extends WorkbenchRoute {
-  model({ id }) {
-    const variables = { id };
-    return this.get("apollo").watchQuery({ query, variables }, "contributor");
-  }
+  model() {}
 }
--- app/routes/workbench/entries.js
+++ app/routes/workbench/entries.js
@@ -1,8 +1,5 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
-import query from "wandertext/gql/queries/entries";
 
 export default class WorkbenchEntriesRoute extends WorkbenchRoute {
-  model() {
-    return this.get("apollo").watchQuery({ query }, "entries");
-  }
+  model() {}
 }
