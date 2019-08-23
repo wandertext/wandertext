d854e17f02e818f801288161ac437f32d1a3d0ce
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-23 09:37:35 -0400

    adapt urlForQuery method to let me grab texts/1/entries

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,20 @@
+/* eslint prefer-rest-params: 1 */
 import DS from "ember-data";
 import config from "wandertext/config/environment";
+import { pluralize } from "ember-inflector";
 
 export default class ApplicationAdapter extends DS.JSONAPIAdapter {
   host = config.apiHost;
+
+  urlForQuery(query, model) {
+    if (query.filter && query.filter.model) {
+      const name = pluralize(query.filter.model.constructor.modelName);
+      const { id } = query.filter.model;
+      const namespace = this.namespace ? `${this.namespace}/` : "";
+      delete query.filter.model;
+      return `${this.host}/${namespace}${name}/${id}/${pluralize(model)}`;
+    }
+
+    return this._super(...arguments);
+  }
 }
