97da937e35121356a315e8408d811d8a2435776f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-26 15:30:27 -0400

    feat: pickup Bāburnama on workbench/index with graphql.

--- app/routes/workbench/index.js
+++ app/routes/workbench/index.js
@@ -1,5 +1,6 @@
 import WorkbenchRoute from "wandertext/routes/workbench";
 import { inject as service } from "@ember/service";
+import query from "wandertext/gql/queries/text.graphql";
 import { hash } from "rsvp";
 
 export default class WorkbenchIndexRoute extends WorkbenchRoute {
@@ -7,7 +8,8 @@ export default class WorkbenchIndexRoute extends WorkbenchRoute {
 
   async model() {
     const contributor = await this.currentContributor.contributor;
-    const text = await this.store.findRecord("text", "baburnama-1530");
+    const variables = { id: "baburnama-1530" };
+    const text = await this.apollo.watchQuery({ query, variables }, "text");
     return hash({ contributor, text });
   }
 }
