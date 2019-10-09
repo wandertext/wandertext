be82ce8abe859fc25391ca8aef1ce48190bb2038
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-10-09 13:52:45 -0400

    feat: implement basic row for creating new entry. fixes #378

--- app/components/entry-grid-controls.js
+++ app/components/entry-grid-controls.js
@@ -1,3 +1,34 @@
 import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
-export default class EntryGridControlsComponent extends Component {}
+export default class EntryGridControlsComponent extends Component {
+  @tracked newShowing = false;
+
+  @tracked model = [];
+
+  constructor(...args) {
+    super(...args);
+    const model = { properties: {} };
+    this.args.columns.map(column => {
+      if (column.valuePath.startsWith("properties.")) {
+        model.properties[column.valuePath.replace("properties.", "")] = null;
+      } else {
+        model[column.valuePath] = "boo";
+      }
+
+      return true;
+    });
+    this.model.push(model);
+  }
+
+  @action
+  toggleNew() {
+    this.newShowing = !this.newShowing;
+  }
+
+  @action
+  focusIn() {
+    return true;
+  }
+}
