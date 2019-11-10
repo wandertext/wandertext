daaa8710a42c4397a030049021e5fcf3b888d506
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-11-09 23:59:42 -0600

    feat: turn off authenticated route mixin

--- app/routes/workbench.js
+++ app/routes/workbench.js
@@ -1,9 +1,6 @@
 import Route from "@ember/routing/route";
-import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 import { queryManager } from "ember-apollo-client";
 
-export default class WorkbenchRoute extends Route.extend(
-  AuthenticatedRouteMixin
-) {
+export default class WorkbenchRoute extends Route {
   @queryManager apollo;
 }
