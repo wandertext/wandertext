092fdac252ba84892c23a702e99f6d8e7348cdfe
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-12-17 13:41:24 -0500

    feat: delete models from mirage. Mirage doesn't care.

--- mirage/models/entry-property.js
+++
@@ -1,6 +0,0 @@
-import { Model, belongsTo } from "ember-cli-mirage";
-
-export default Model.extend({
-  text: belongsTo("text", { async: false }),
-  contributor: belongsTo("contributor", { async: false })
-});
