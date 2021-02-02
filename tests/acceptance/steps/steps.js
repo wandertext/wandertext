93445fc74724f58cdf4ccef923679fa426c404c2
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-02-02 16:09:53 -0500

    lint: Remove ESLint comments from steps file.

--- tests/acceptance/steps/steps.js
+++ tests/acceptance/steps/steps.js
@@ -1,11 +1,8 @@
-/* eslint prefer-arrow-callback: "off", import/no-anonymous-default-export: "off" */
 import yadda from "yadda";
 // import { visit } from "@ember/test-helpers";
 
 export default function (assert) {
   return yadda.localisation.default
     .library()
-    .given("I am a creator", function () {
-      assert.ok(true, this.step);
-    });
+    .given("I am a creator", () => assert.ok(true, this.step));
 }
