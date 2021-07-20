a6df4c2fc90dd0e010d3512b7a2864855e48d921
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-07-20 16:49:21 -0400

    Strip down tailwind config and define colors. Fixes #716

---
+++ app/tailwind/colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  white: "#ffffff",
+  black: "#000000",
+  primary: "#012169",
+  primaryDark: "#011541",
+  columbiaBlue: "#B9D9EB",
+  columbiaBlueLight: "#EFF6FB",
+  error: "#FE5D26",
+  errorLight: "#FFE1D6",
+  warning: "#F2C078",
+  warningLight: "#F9E5C7",
+  gray: "#C4C4C4",
+  grayLight: "#F0F0F0",
+};
