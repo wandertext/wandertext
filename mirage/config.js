3527d0348933643ea0473cfae4c6cb81f4d54014
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-09-30 13:20:13 -0400

    deps: Mirage now working.

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,4 @@
 export default function () {
-  // These comments are here to help you get started. Feel free to delete them.
   /*
     Config (with defaults).
 
@@ -8,15 +7,11 @@ export default function () {
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-  /*
-    Shorthand cheatsheet:
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
+  this.resource("contributor");
+  this.resource("entry-property");
+  this.resource("entry");
+  this.resource("flag");
+  this.resource("place");
+  this.resource("text");
 }
