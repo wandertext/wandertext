071ffc839378a121322d214b8e7e728a87ba44b7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-10-05 15:39:54 -0400

    feat: Disable touch for now.

--- app/modifiers/on-swipe-left.js
+++ app/modifiers/on-swipe-left.js
@@ -9,19 +9,30 @@ import { modifier } from "ember-modifier";
 import Hammer from "hammerjs";
 
 export default modifier(function onSwipeLeft(element /* , params, hash */) {
-  const hammer = new Hammer.Manager(element);
+  // Const hammer = new Hammer.Manager(element);
 
-  hammer.add(
-    new Hammer.Swipe({
-      event: "doubleswipeleft",
-      pointers: 2,
-      direction: Hammer.DIRECTION_LEFT,
-    })
-  );
+  const hammer = new Hammer(element);
 
-  hammer.on("doubleswipeleft", () => {
-    alert("doubleswipeleft");
-  });
+  hammer.on("pan", ev => alert("panning"));
 
-  return hammer.off("doubleswipeleft");
+  // Element.addEventListener("click", () => alert("event click"));
+
+  // hammer.add(
+  //   new Hammer.Swipe({
+  //     event: "doubleswipeleft",
+  //     pointers: 2,
+  //     direction: Hammer.DIRECTION_LEFT,
+  //   })
+  // );
+
+  // hammer.on("doubleswipeleft", () => {
+  //   alert("doubleswipeleft");
+  // });
+
+  // hammer.on("swipeleft", () => {
+  //   alert("swipeleft");
+  // });
+
+  // return hammer.off("doubleswipeleft");
+  return hammer.off("pan");
 });
