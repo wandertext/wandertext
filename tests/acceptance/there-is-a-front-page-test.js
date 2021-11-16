04df57e012127aaadb6c9482eeef6d5178b35a68
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-11-15 23:20:16 -0500

    tests: create a basic there-is-a-front-page test

---
+++ tests/acceptance/there-is-a-front-page-test.js
@@ -0,0 +1,15 @@
+import { module, test } from "qunit";
+import { visit } from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
+
+module("Acceptance | there is a front page", function (hooks) {
+  setupApplicationTest(hooks);
+
+  test("visiting /", async function (assert) {
+    await visit("/");
+
+    const title = this.element.querySelector("h1");
+
+    assert.dom(title).hasText("Wandertext");
+  });
+});
