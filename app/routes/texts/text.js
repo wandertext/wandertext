aa89fa6077446b59007b86aefd4ae5dbb85d688b
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-11-16 15:48:49 -0500

    chore(deps): Update service injection for one more route

--- app/routes/texts/text.js
+++ app/routes/texts/text.js
@@ -1,6 +1,9 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
 export default class TextsTextRoute extends Route {
+  @service store;
+
   async model({ textId }) {
     return this.store.findRecord("text", textId);
   }
