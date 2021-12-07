1f075da9620d06646d91adbc93259a83923df4e3
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-12-07 15:47:20 -0500

    tests: Add attribute empty and show menu tests for fab

--- app/components/fab.js
+++ app/components/fab.js
@@ -5,6 +5,14 @@ import { inject as service } from "@ember/service";
 export default class FabComponent extends Component {
   @service menuState;
 
+  get icon() {
+    if (this.args.icon) {
+      return this.args.icon;
+    }
+
+    return "menu";
+  }
+
   actions = {
     menu: this.menuState.toggleMenu,
   };
--- tests/integration/components/fab-test.js
+++ tests/integration/components/fab-test.js
@@ -1,7 +1,8 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
+import { click, render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
+import { getOwner } from "@ember/application";
 
 module("Integration | Component | fab", hooks => {
   setupRenderingTest(hooks);
@@ -10,8 +11,26 @@ module("Integration | Component | fab", hooks => {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Fab @icon="menu" />`);
+    await render(hbs`<Fab @icon="text" />`);
+
+    assert.dom(this.element).hasTextContaining("Text Icon");
+  });
+
+  test("it renders the menu button when no @icon is defined", async function (assert) {
+    await render(hbs`<Fab />`);
 
     assert.dom(this.element).hasTextContaining("Menu Icon");
+  });
+
+  test("it shows the menu when clicked with the menu icon", async function (assert) {
+    const owner = getOwner(this);
+    const menuState = owner.lookup("service:menuState");
+    assert.false(menuState.menuVisible);
+    await render(hbs`<Fab />`);
+
+    assert.dom(this.element).hasTextContaining("Menu Icon");
+    await click("button");
+
+    assert.true(menuState.menuVisible);
   });
 });
