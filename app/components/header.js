1244d6842ff5638ed957c9a182035213ede545e8
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-12-07 16:45:38 -0500

    feat: Add back button to Header. Fixes #889
    Also updated the tests and logic to allow for unset attributes.

--- app/components/header.js
+++ app/components/header.js
@@ -5,7 +5,27 @@ import { inject as service } from "@ember/service";
 export default class HeaderComponent extends Component {
   @service menuState;
 
+  get icon() {
+    if (this.args.icon) {
+      return this.args.icon;
+    }
+
+    return "logo";
+  }
+
+  get title() {
+    if (this.args.title) {
+      return this.args.title;
+    }
+
+    return "Wandertext";
+  }
+
   @action handleMenuClick() {
     this.menuState.menuVisible = true;
+  }
+
+  @action goBack() {
+    window.history.back(); // eslint-disable-line no-undef
   }
 }
--- tests/integration/components/header-test.js
+++ tests/integration/components/header-test.js
@@ -7,11 +7,18 @@ module("Integration | Component | header", function (hooks) {
   setupRenderingTest(hooks);
 
   test("it renders", async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<Header @title="Test Title" />`);
 
     assert.dom(this.element).hasTextContaining("Test Title");
   });
+
+  test("it renders with Wandertext icon and title with no attributes set", async function (assert) {
+    await render(hbs`<Header />`);
+
+    assert.dom("h1").hasText("Wandertext");
+    assert.dom(this.element).hasTextContaining("Wandertext Logo");
+  });
+
+  // This should be an acceptance test.
+  test.skip("it features a back button");
 });
