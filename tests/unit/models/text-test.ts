ee3c1d3bd0ef2c3016f881f785bf368f4ef9da7a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-15 17:20:32 -0400

    tests: Add appropriate typing for Store.

--- tests/unit/models/text-test.ts
+++ tests/unit/models/text-test.ts
@@ -1,12 +1,14 @@
 import { module, test } from "qunit";
 import { setupTest } from "ember-qunit";
+import Store from "@ember-data/store";
 
 module("Unit | Model | text", hooks => {
   setupTest(hooks);
 
   // Replace this with your real tests.
   test("it exists", function (assert) {
-    const store = this.owner.lookup("service:store");
+    const store = this.owner.lookup("service:store") as Store;
+    store.createRecord;
     const model = store.createRecord("text", {});
     assert.ok(model);
   });
