34b785e708da0483b652066ce86ab432bbe25995
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-22 21:16:10 -0400

    lint: lint

--- tests/integration/components/map/leaflet-container-test.ts
+++ tests/integration/components/map/leaflet-container-test.ts
@@ -12,7 +12,7 @@ module("Integration | Component | map/leaflet-container", function (hooks) {
 
     await render(hbs`<Map::LeafletContainer />`);
 
-    assert.dom(this.element).hasText("");
+    assert.dom().hasText("");
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module("Integration | Component | map/leaflet-container", function (hooks) {
       </Map::LeafletContainer>
     `);
 
-    assert.dom(this.element).hasText("template block text");
+    assert.dom().hasText("template block text");
   });
 });
