c28982dff00a0138d3bdd4467262e9c85b6c3f80
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-29 16:25:47 -0400

    feat(backend): Add '/api' namespace and change config to new format. Fixes #980

--- mirage/config.js
+++ mirage/config.js
@@ -1,17 +1,20 @@
-export default function () {
-  /*
-    Config (with defaults).
+import { discoverEmberDataModels } from "ember-cli-mirage";
+import { createServer } from "miragejs";
 
-    Note: these only affect routes defined *after* them!
-  */
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+export default function (config) {
+  let finalConfig = {
+    ...config,
+    models: { ...discoverEmberDataModels(), ...config.models },
+    routes() {
+      this.namespace = "/api";
+      this.resource("contributor");
+      this.resource("entry-property");
+      this.resource("entry");
+      this.resource("flag");
+      this.resource("place");
+      this.resource("text");
+    },
+  };
 
-  this.resource("contributor");
-  this.resource("entry-property");
-  this.resource("entry");
-  this.resource("flag");
-  this.resource("place");
-  this.resource("text");
+  return createServer(finalConfig);
 }
