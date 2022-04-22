93dda4021c5abf347f3ca373efc160581abafc9c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-22 13:01:15 -0400

    feat: Update ember-leaflet and add new version of mirage config.

--- mirage/config.js
+++ mirage/config.js
@@ -1,29 +1,29 @@
-// import {
-//   discoverEmberDataModels,
-//   // applyEmberDataSerializers,
-// } from "ember-cli-mirage";
-// import { createServer } from "miragejs";
+import {
+  discoverEmberDataModels,
+  //   // applyEmberDataSerializers,
+} from "ember-cli-mirage";
+import { createServer } from "miragejs";
 
-export default function (/*config*/) {
-  //   let finalConfig = {
-  //     ...config,
-  //     // Remove discoverEmberDataModels if you do not want ember-cli-mirage to auto discover the ember models
-  //     models: { ...discoverEmberDataModels(), ...config.models },
-  //     // uncomment to opt into ember-cli-mirage to auto discover ember serializers
-  //     // serializers: applyEmberDataSerializers(config.serializers),
-  //     routes,
-  //   };
+export default function (config) {
+  let finalConfig = {
+    ...config,
+    //     // Remove discoverEmberDataModels if you do not want ember-cli-mirage to auto discover the ember models
+    models: { ...discoverEmberDataModels(), ...config.models },
+    //     // uncomment to opt into ember-cli-mirage to auto discover ember serializers
+    //     // serializers: applyEmberDataSerializers(config.serializers),
+    routes() {
+      // These will go in the routes function in the future.
+      this.namespace = "/api";
+      this.resource("contributors");
+      this.resource("entry-properties");
+      this.resource("entries");
+      this.resource("flags");
+      this.resource("places");
+      this.resource("texts");
+    },
+  };
 
-  // These will go in the routes function in the future.
-  this.namespace = "/api";
-  this.resource("contributors");
-  this.resource("entry-properties");
-  this.resource("entries");
-  this.resource("flags");
-  this.resource("places");
-  this.resource("texts");
-
-  //   return createServer(finalConfig);
+  return createServer(finalConfig);
 }
 
 // function routes() {
