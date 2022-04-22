03ae36010718ae1a03dd44f51ddb9d0ab2e3de94
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-22 16:39:37 -0400

    tests: Migrate some component tests to ts.

--- tests/integration/components/fab-test.ts
+++ tests/integration/components/fab-test.ts
@@ -2,7 +2,7 @@ import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { click, render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
-import { getOwner } from "@ember/application";
+import MenuState from "wandertext/services/menu-state";
 
 module("Integration | Component | fab", hooks => {
   setupRenderingTest(hooks);
@@ -13,22 +13,21 @@ module("Integration | Component | fab", hooks => {
 
     await render(hbs`<Fab @icon="text" />`);
 
-    assert.dom(this.element).hasTextContaining("Text Icon");
+    assert.dom().hasTextContaining("Text Icon");
   });
 
   test("it renders the menu button when no @icon is defined", async function (assert) {
     await render(hbs`<Fab />`);
 
-    assert.dom(this.element).hasTextContaining("Menu Icon");
+    assert.dom().hasTextContaining("Menu Icon");
   });
 
   test("it shows the menu when clicked with the menu icon", async function (assert) {
-    const owner = getOwner(this);
-    const menuState = owner.lookup("service:menuState");
+    const menuState = this.owner.lookup("service:menuState") as MenuState;
     assert.false(menuState.menuVisible);
     await render(hbs`<Fab />`);
 
-    assert.dom(this.element).hasTextContaining("Menu Icon");
+    assert.dom().hasTextContaining("Menu Icon");
     await click("button");
 
     assert.true(menuState.menuVisible);
---
+++ tests/integration/components/header-test.ts
@@ -0,0 +1,36 @@
+import { module, test, skip } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import faker from "@faker-js/faker";
+
+module("Integration | Component | header", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("it renders", async function (assert) {
+    await render(hbs`<Header @title="Test Title" />`);
+
+    assert.dom().hasTextContaining("Test Title");
+  });
+
+  test("it renders with Wandertext icon and title with no attributes set", async function (assert) {
+    await render(hbs`<Header />`);
+
+    assert.dom("h1").hasText("Wandertext");
+    assert
+      .dom("[data-test-header-center]")
+      .hasTextContaining("Wandertext Logo");
+  });
+
+  test("it renders with place icon and random title when set", async function (assert) {
+    const fakeTitle = faker.random.words(2);
+    this.set("title", fakeTitle);
+    await render(hbs`<Header @title={{this.title}} @icon="place" />`);
+
+    assert.dom("h1").hasText(fakeTitle);
+    assert.dom("[data-test-header-center]").hasTextContaining("Place Icon");
+  });
+
+  // This should be an acceptance test.
+  skip("it features a back button");
+});
