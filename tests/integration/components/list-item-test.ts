8f23849c4a224b1095d24a8372905cdcd04e7567
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-24 00:03:46 -0400

    test: Expand ListItem test and split Text and Place branches

--- tests/integration/components/list-item-test.ts
+++ tests/integration/components/list-item-test.ts
@@ -2,9 +2,10 @@ import { module, skip, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
-import { MirageTestContext } from "wandertext";
 import Store from "@ember-data/store";
 import { setupMirage } from "ember-cli-mirage/test-support";
+import { MirageTestContext } from "wandertext";
+import TextModel from "wandertext/models/text";
 
 module("Integration | Component | list-item", function(hooks) {
   setupRenderingTest(hooks);
@@ -27,36 +28,41 @@ module("Integration | Component | list-item", function(hooks) {
     assert.dom("[data-test-info-box]").includesText(text.name);
   });
 
-  skip("it has a map that shows associated places", async function(this: MirageTestContext, assert) {
-    const store = this.owner.lookup("service:store") as Store;
-    const text = this.server.create("text");
-    this.server.createList("place", 10);
-    const places = this.server.schema.all("place");
-    this.server.createList("entry", 10, {
-      text,
-      place: () => places.models[Math.floor(Math.random() * 10)],
-    } as object);
-
-    const entries = await store.findAll("entry", { include: "text,place" });
-    const entriesArray = entries.toArray();
-    console.log(entriesArray);
-
-    assert.ok(true);
-  });
-
-  // skip("it has a markers getter that returns an array of places", async function(this: MirageTestContext, assert){
-  //   const store = this.owner.lookup("service:store") as Store;
-  //   const text = this.server.create("text");
-  //   this.server.createList("place", 10);
-  //   const places = this.server.schema.all("place");
-  //   this.server.createList("entry", 10, {
-  //     text,
-  //     place: () => places.models[Math.floor(Math.random() * 10)],
-  //   } as object);
-
-  // });
-
   skip("it has a @model argument");
   skip("it has a @linkToRoute argument");
   skip("it has a mapVisible property");
+
+  module("when @model is Place", function() {
+    skip("it has a markers getter that returns the place itself");
+  });
+
+  module("when @model is Text", function(hooks) {
+    hooks.beforeEach(async function(this: MirageTestContext) {
+      const text = this.server.create("text", {
+        name: "Book One",
+        markdownName: "_Book One Italics_",
+      } as object);
+      const places = this.server.createList("place", 10);
+      this.server.createList("entry", 10, {
+        text,
+        place: () => places[Math.floor(Math.random() * 10)],
+      } as object);
+      const store = this.owner.lookup("service:store") as Store;
+      const model = await store.findRecord("text", 1, {
+        include: "entries,entries.place",
+      });
+      this.set("model", model);
+    });
+
+    test("it shows the Text's name as rendered markdown", async function(assert) {
+      await render(hbs`<ListItem @model={{this.model}} />`);
+
+      assert.dom("em").hasText("Book One Italics");
+    });
+
+    test("it has a markers getter that returns an array of places", async function(this: MirageTestContext, assert) {
+      const text = this.model as TextModel;
+      assert.ok(text);
+    });
+  });
 });
--- types/wandertext/mirage-test-context.ts
+++ types/wandertext/mirage-test-context.ts
@@ -1,6 +1,9 @@
 import { type TestContext } from "@ember/test-helpers";
 import { Server } from "miragejs";
+import PlaceModel from "wandertext/models/place";
+import TextModel from "wandertext/models/text";
 
 export default interface MirageTestContext extends TestContext {
   server: Server;
+  model: PlaceModel | TextModel | PlaceModel[] | TextModel[];
 }
