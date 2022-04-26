c5ad44954d54d994256e223f2a9efdaa3a895df2
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-26 16:57:13 -0400

    test: Test that markers get drawn on Map.

--- tests/integration/components/map-test.ts
+++ tests/integration/components/map-test.ts
@@ -12,12 +12,39 @@ module("Integration | Component | map", hooks => {
     assert.dom().includesText("");
   });
 
-  skip("it expects a @fullScreen argument");
+  test("it draws a map with one marker for every Marker", async function (assert) {
+    const markers = [1, 2, 3, 4, 5, 6, 7].map(() => {
+      return {
+        createdAt: new Date(),
+        name: "some name",
+        latitude: Math.random() * 140 - 70,
+        longitude: Math.random() * 340 - 170,
+      };
+    });
+
+    const count = markers.length;
+
+    this.set("markers", markers);
+
+    await render(hbs`<Map @markers={{this.markers}} />`);
+
+    assert.dom(".leaflet-marker-icon").exists({ count });
+  });
+
+  skip("it is the full window size when @fullScreen is true", async function (assert) {
+    await render(hbs`<Map @fullScreen={{true}} />`);
+
+    if (this.element) {
+      console.log("there is an element");
+      // const map = this.element.querySelector("[data-test-map-full-screen]");
+    }
+
+    assert.ok(true);
+  });
+
   skip("it expects a @defaultModalOpen argument");
   skip("it expects a @modalContentComponent argument");
-  skip("it expects a @markers argument");
 
-  skip("it is the full window size when @fullScreen is true");
   skip("it auto-opens a modal when @defaultModalOpen is true");
   skip("The modal content component is...");
 });
