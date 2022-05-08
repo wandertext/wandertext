62a92bece2562328afef92f244fde6edac74fc43
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-05-08 15:55:39 -0400

    Replace did-insert with action with dedicated open-modal modifier.

---
+++ app/modifiers/open-modal.ts
@@ -0,0 +1,24 @@
+import Modifier from "ember-modifier";
+import { service } from "@ember/service";
+import type Modals from "ember-promise-modals";
+
+export interface OpenModalModifierSignature {
+  Element: HTMLDivElement;
+  Args: {
+    Positional: [modalContent: string];
+  };
+}
+
+export default class OpenModalModifier extends Modifier<OpenModalModifierSignature> {
+  @service declare modals: Modals;
+
+  modify(_element: HTMLElement, [modalContent]: [string]) {
+    this.modals.open(`modals/${modalContent}`);
+  }
+}
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    "open-modal": typeof OpenModalModifier;
+  }
+}
