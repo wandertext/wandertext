9b37e5cff2aed844a632766c68943a3cb801e190
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2024-01-30 10:09:45 -0500

    chore(deps): Install ember-cli-typescript

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,6 @@
 // Types for compiled templates
-declare module "wandertext/templates/*" {
-  import { TemplateFactory } from "htmlbars-inline-precompile";
+declare module 'wandertext/templates/*' {
+  import { TemplateFactory } from 'ember-cli-htmlbars';
 
   const tmpl: TemplateFactory;
   export default tmpl;
